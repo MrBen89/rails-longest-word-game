390fe21f7cf9377eee26aa1555f692151950c074
Ben Pearson <benpearson61@googlemail.com>
2024-12-17 21:48:09 +0900

    done....ish

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ("A".."Z").to_a.sample(10)
+  end
+
+  def score
+    require 'open-uri'
+    url = "https://dictionary.lewagon.com/"
+    html_content = URI.parse("#{url}#{params[:word]}").read
+    doc = JSON.parse(html_content)
+    p params
+    word_in_letters = true
+    params[:word].upcase.chars.each do |letter|
+      unless params[:letters].include? letter
+        word_in_letters = false
+      end
+    end
+
+    if word_in_letters == false
+      @message = "#{params[:word]} can't be made with those letters."
+    elsif doc["found"] == false
+      @message = "#{params[:word]} is not a valid word!"
+    else
+      @message = "#{params[:word]} is worth #{doc["length"].to_i * doc["length"].to_i} points"
+    end
+  end
+end
