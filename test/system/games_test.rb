dde94edb9f4bb58a80d25b4b324393c5dd9e0dda
Ben Pearson <benpearson61@googlemail.com>
2024-12-19 20:55:55 +0900

    rails new

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -6,4 +6,22 @@ class GamesTest < ApplicationSystemTestCase
     assert test: "New game"
     assert_selector "li", count: 10
   end
+  test "Submitting a random word raises an error" do
+    visit new_url
+    fill_in "word", with: "Mypineapplehurts!"
+    click_on "Submit!"
+
+    assert_text "Mypineapplehurts! can't be made with those letters."
+  end
+  test "Submitting a one letter consonant shows invalid word" do
+    visit new_url
+    p all(:css, "li")[0].text(:all)
+    fill_in "word", with: "b"
+
+    click_on "Submit!"
+
+    p URI.parse(current_url).query
+
+    assert_text "b is not a valid word!"
+  end
 end
